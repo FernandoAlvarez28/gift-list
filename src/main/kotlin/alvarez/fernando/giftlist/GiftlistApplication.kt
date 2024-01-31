f25ecbec5e5d0d06e3153b23a64ccbc5696be4bc
Fernando Alvarez <fernando44ca@gmail.com>
2024-01-30 22:55:58 -0300

    feat: Initial Gradle/Spring Initializr setup

---
+++ src/main/kotlin/alvarez/fernando/giftlist/GiftlistApplication.kt
@@ -0,0 +1,11 @@
+package alvarez.fernando.giftlist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GiftlistApplication
+
+fun main(args: Array<String>) {
+	runApplication<GiftlistApplication>(*args)
+}
---
+++ src/test/kotlin/alvarez/fernando/giftlist/GiftlistApplicationTests.kt
@@ -0,0 +1,13 @@
+package alvarez.fernando.giftlist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GiftlistApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
