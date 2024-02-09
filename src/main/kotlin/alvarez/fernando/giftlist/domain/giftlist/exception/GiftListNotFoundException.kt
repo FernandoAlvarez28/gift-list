10097d1aba402d628eb1f00fabbddf6ca2dd345c
Fernando Alvarez <fernando44ca@gmail.com>
2024-02-09 00:50:44 -0300

    chore(GiftListNotFoundException): Add `NOT_FOUND` status annotation

--- src/main/kotlin/alvarez/fernando/giftlist/domain/giftlist/exception/GiftListNotFoundException.kt
+++ src/main/kotlin/alvarez/fernando/giftlist/domain/giftlist/exception/GiftListNotFoundException.kt
@@ -1,3 +1,7 @@
 package alvarez.fernando.giftlist.domain.giftlist.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class GiftListNotFoundException : Exception()
